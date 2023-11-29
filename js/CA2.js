ebb42cc1c7b6292f266c8b55417c486834614eb3
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2023-11-29 21:19:57 +0000

    Add files via upload

---
+++ js/CA2.js
@@ -0,0 +1,55 @@
+function bear()
+{
+      alert("Introducing Bruno Adventurous," +
+            "the brave bear explorer! At 6 years old," +
+            "Bruno comes from the lush forests of Canada." +
+            "His favorite pastime is mapping out new trails" + 
+            "in the zoo and discovering hidden treasures." + 
+            "Bruno loves to indulge in honey picnics," + 
+            "showcasing his sweet tooth and warm-hearted spirit.");
+}
+
+function bunny()
+{
+      alert("Ricardo Entrepreneur, the business-savvy rabbit," +
+            "is 3 years old and hails from the green hills of Ireland." +
+            "When not busy overseeing zoo operations," +
+            "Ricardo enjoys nibbling on clover snacks" + 
+            "and strategizing his next big carrot investment." +
+            "His sharp mind and quick hops make him" +
+            "an invaluable member of the zoo's administrative team.");
+}
+
+function cops()
+{
+      alert("Meet Penelope and Pablo," +
+            "the dynamic duck duo! Penelope, 4 years old," +
+            "hails from the picturesque ponds of France," + 
+            "while Pablo, 5 years old, quacks proudly" +
+            "from the vibrant landscapes of Spain." +
+            "When they're not patrolling the zoo," +
+            "you can find Penelope splashing in" +
+            "her favorite pond and" +
+            "Pablo enjoying a siesta under the warm sun.")
+}
+
+function cat()
+{
+      alert("Dr. Mia Purring, the caring kitten," + 
+            "is 1 year old and originally from the cozy homes of Japan." + 
+            "Aside from her duties as the zoo's medical expert," +
+            "Mia enjoys practicing her purr therapy on stressed-out animals." +
+            "Her gentle demeanor and love for soothing melodies" +
+            "make her the go-to doctor for all the zoo's residents.")
+}
+
+function dog()
+{
+      alert("Bolt Eager, the energetic puppy," +
+            "is a 2-year-old bundle of joy" +
+            "from the lively streets of the United States." +
+            "Bolt's favorite hobby is chasing his tail" +
+            "and exploring every nook and cranny of the zoo." +
+            "His boundless enthusiasm and wagging tail bring" + 
+            "smiles to visitors of all ages.")
+}
